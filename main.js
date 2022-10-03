d12745c277c1e5280d2ac226e7e3edabe1d3b024
gilberto <gilbertogalindez96@gmail.com>
2022-10-02 23:14:42 -0400

    implementation of getNodeByIndex, insert and remove

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import { mobsArray } from './mobsArray.js';
 import { mobHashTable } from './mobHashTable.js';
+import { MySinglyLinkedList } from './singlyLinkedList.js';
+
+let myLinkedList = new MySinglyLinkedList(1);
+
+myLinkedList.append(2);
+myLinkedList.append(3);
+myLinkedList.append(4);
+
+myLinkedList.preappend(0);
+myLinkedList.insert(2, 'mi insert en el index 2');
+myLinkedList.remove(0);
+myLinkedList.remove(0);
+
+console.log(myLinkedList);
--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,21 +1,3 @@
-/* 1-- > 2-- > 3-- > 4-- > 5-- > null;
-
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null,
-        },
-      },
-    },
-  },
-}; */
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -47,6 +29,54 @@ class MySinglyLinkedList {
     this.length++;
     return this;
   }
+  insert(index, entry) {
+    //increase the length
+    this.length++;
+    // create a new node
+    const node = new Node(entry);
+    // if the length is = to 1 just append the new node to the list
+    if (index >= this.length) {
+      this.append(node);
+      return this;
+    }
+    // I get the node that is about to be replace with the new one
+    const oldNode = this.getNodeByIndex(index);
+    // I link the old node with the new one in order to not lose it
+    node.next = oldNode;
+    // I look for the previous node in order to link my new node to it
+    const previuosNode = this.getNodeByIndex(index - 1);
+    // i link it
+    previuosNode.next = node;
+    //and that's it
+    return this;
+  }
+  getNodeByIndex(index) {
+    let nodeRequired = this.head;
+    let counter = 0;
+
+    while (counter !== index) {
+      nodeRequired = nodeRequired.next;
+      counter++;
+    }
+    return nodeRequired;
+  }
+  remove(index) {
+    //decrease the length
+    this.length--;
+    // I get the next node to it
+    const nextNode = this.getNodeByIndex(index + 1);
+    // index = 0 is an special case
+    if (index === 0) {
+      this.head = nextNode;
+      return this;
+    }
+    // I get the previous node to it
+    const previuosNode = this.getNodeByIndex(index - 1);
+    // I link the next node to the previous one
+    previuosNode.next = nextNode;
+    // and that's it
+    return this;
+  }
 }
 
-let myLinkedList = new MySinglyLinkedList(1);
+export { MySinglyLinkedList };
